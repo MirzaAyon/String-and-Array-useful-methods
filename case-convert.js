3bb6fc4299244fea7818a91c3ccc84296f710636
MirzaAyon <ayonmirza98@gmail.com>
2022-06-17 20:06:38 +0600

    case convert added

---
+++ case-convert.js
@@ -0,0 +1,13 @@
+const anthen = "amar sonar Bangla ami tomae Bhalobashi";
+//ekhane bhalo word ta ache kina
+
+const search = 'bhalo';
+
+
+
+const userInput = 'BlackPink'
+
+const savedUserName = 'blackPink';
+if (userInput == savedUserName) {
+    console.log('user exists');
+}
---
+++ case2.js
@@ -0,0 +1,11 @@
+const anthen = "amar sonar Bangla ami tomae Bhalobashi";
+//ekhane bhalo word ta ache kina
+
+const search = 'bhalo';
+const userInput = 'BlackPink'
+console.log(userInput);
+console.log(userInput.toLowerCase());
+const savedUserName = 'blackPink';
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log('user exists');
+}
