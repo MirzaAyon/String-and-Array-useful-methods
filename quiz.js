0179d5a970f6ffdae142a445ae8967cf1b3a93b7
MirzaAyon <ayonmirza98@gmail.com>
2022-06-18 14:17:08 +0600

    done

---
+++ quiz.js
@@ -0,0 +1,34 @@
+//quiz 2
+// const task = "Practice Makes a person perfect";
+// // const index = task.toLowerCase().indexOf('a'); //2
+// // const index = task.toLowerCase().indexOf('M'); //-1
+// // const index = task.toLowerCase().indexOf(M); //error
+// console.log(index);
+
+//quiz 3
+// const str1 = 'Bangladesh is a beautiful country.'; console.log(str1.endsWith("country")); //false
+
+// quiz - 4
+// Which one will give you the word - "love" ?
+
+// const str= 'I love coding more than eating.';
+//str.slice(2,6);
+
+// quiz - 8
+// What error message will you get for the following code (try it out)?
+
+// for (let i==0; i<10;i++){} //syntex error
+
+
+// quiz 10
+
+// Which html tag attribute will you use to get a tooltip? //
+
+// 11
+// How can you put all the elements of an array in a string? // using join method
+
+//12
+// Suppose you have two string variables: str1, str2. How can you add both the string so that str2 comes first? //str2.concat(str1)
+
+// 13
+How can you get the current date using js Date objects ? // console.log(Date.now())
